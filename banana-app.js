b98603b593a9fc033b0f69d57f2523e4550748b2
Vai8hav <vaibhav.basantani@gmail.com>
2022-09-01 08:40:47 +0530

    refactor: removed comments
    These comments were for personal understanding which were removed

--- banana-app.js
+++ banana-app.js
@@ -1,20 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
-//Here we are telling the browser which element should it refer to using its id and storing that refernece into a variable
 
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-
-
-// console.log("Clicked!");   Prints 'clicked' when someone presses the button.
-// console.log("input", txtInput.value);    When there is some 'input', show that input to the user.
-
-// outputDiv.innerText = "jhfgafihafh"   this is the translated value
-
-// txtInput.value (prints the input that the user wrote)
-
-// Here we are telling the browser to add an event, meaning, something should happen when we 'click' the button. Then we pass a special function and then put up what actually we do want, which is printing the string in this case.
-
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
